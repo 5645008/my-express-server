3017066395365156ca4d858a81e27fe26ea0c980
5645008 <kangbumsuck@gmail.com>
2024-11-04 00:01:28 +0900

    회원가입,로그인 테스트4

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql2");  // MySQL 모듈 불러오기
 const cors = require('cors'); // cors 패키지 불러오기
+const bodyParser = require("body-parser");
 
 const app = express();
 app.use(cors({
   origin: '*'  // 모든 도메인에서의 요청을 허용
 }));
 app.use(express.json());  // JSON 요청을 처리할 수 있도록 설정
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // 포트 설정 (3000번 포트를 기본값으로 사용)
 app.set("port", process.env.PORT || 3000);
