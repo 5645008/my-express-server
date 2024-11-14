e28a1ecdbc36591f2487eb708ee7a2016f94abb7
5645008 <kangbumsuck@gmail.com>
2024-11-14 22:11:49 +0900

    메인화면 이름 테스트

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -18,6 +18,7 @@ function Login() {
       });
 
       if (response.data.success) {
+        localStorage.setItem('user_id', response.data.user_id);  // 서버에서 받은 user_id 저장
         navigate('/main'); // 홈 페이지로 이동
       } else {
         setErrorMessage(response.data.message); // 로그인 실패 메시지 설정
