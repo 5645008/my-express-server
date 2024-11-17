35b711161b57a7bdd72f8a3e053546c0a305608b
5645008 <kangbumsuck@gmail.com>
2024-11-17 13:33:59 +0900

    텍스트스캔

--- my-app/src/pages/Camera.js
+++ my-app/src/pages/Camera.js
@@ -1,39 +1,24 @@
-import React, { useRef, useState } from 'react';
+// components/Camera.js
+import React, { useRef } from 'react';
 import Webcam from 'react-webcam';
 
 const Camera = ({ onCapture }) => {
   const webcamRef = useRef(null);
-  const [image, setImage] = useState(null);
-
-  const videoConstraints = {
-    facingMode: 'environment', // 모바일에서 후면 카메라 사용
-    width: 1280,
-    height: 720,
-  };
 
   const captureImage = () => {
-    if (webcamRef.current) {
-      const capturedImage = webcamRef.current.getScreenshot();
-      if (capturedImage) {
-        setImage(capturedImage);
-        onCapture(capturedImage); // 부모로 이미지 전달
-      } else {
-        console.error('Failed to capture image');
-      }
-    }
+    const imageSrc = webcamRef.current.getScreenshot();
+    onCapture(imageSrc);
   };
 
   return (
     <div>
-      <h2>Capture Image</h2>
       <Webcam
         audio={false}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
-        videoConstraints={videoConstraints}
+        width={300}
       />
       <button onClick={captureImage}>Capture Image</button>
-      {image && <img src={image} alt="Captured" />}
     </div>
   );
 };
--- my-app/src/pages/TextScanPage.js
+++ my-app/src/pages/TextScanPage.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import Camera from './Camera';
-import ImagePreview from './ImagePreview';
-import TextScanner from './TextScanner';
+// TextScanPage.js
+import React  from 'react';
+import Camera  from './Camera';
+import TextScanner  from './TextScanner';
+import ImagePreview  from './ImagePreview';
+import useCamera  from './useCamera';
 
-const TextScanPage = () => {
-  const [image, setImage] = useState(null);
 
-  const handleCapture = (capturedImage) => {
-    setImage(capturedImage); // 이미지 캡처 후 상태 업데이트
-  };
+
+function TextScanPage() {
+  const { image, captureImage } = useCamera();
+  console.log({ image, captureImage });
 
   return (
-    <div>
-      <h1>Text Scanner</h1>
-      {/* Camera 컴포넌트: 이미지 캡처 기능 */}
-      <Camera onCapture={handleCapture} />
-      
-      {/* ImagePreview 컴포넌트: 캡처된 이미지 미리보기 */}
-      <ImagePreview image={image} />
-
-      {/* TextScanner 컴포넌트: 이미지에서 텍스트 추출 */}
-      {image && <TextScanner image={image} />}
+    <div className="TextScanPage">
+      <h1>Text Scanner App</h1>
+      <Camera onCapture={captureImage} />
+      {image && (
+        <>
+          <ImagePreview image={image} />
+          <TextScanner image={image} />
+        </>
+      )}
     </div>
   );
-};
+}
 
 export default TextScanPage;
