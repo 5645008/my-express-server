6619335d5eb315b4c3cb369a17d253408ad74d3c
5645008 <kangbumsuck@gmail.com>
2024-11-24 13:15:38 +0900

    folders

--- my-app/src/pages/ScanResult.js
+++ my-app/src/pages/ScanResult.js
@@ -1,4 +1,4 @@
-// TextScan/ScanResult.js
+// ScanResult.js
 import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import '../css/ScanResult.styled.css';
