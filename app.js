818fbb4ddcecc4c0a4021eba728a5df44c57c3ed
5645008 <kangbumsuck@gmail.com>
2024-12-09 20:21:16 +0900

    회원가입api수정

--- app.js
+++ app.js
@@ -78,10 +78,10 @@ db.query('SELECT 1', (err, results) => {
 
 // 회원가입 API
 app.post('/api/signup', (req, res) => {
-  const { user_id, user_password, user_age, user_disease, user_gender } = req.body;
+  const { user_id, user_password, user_name, user_age, user_disease, user_gender } = req.body;
 
-  const query = 'INSERT INTO user (user_id, user_password, user_age, user_disease, user_gender) VALUES (?, ?, ?, ?, ?)';
-  const values = [user_id, user_password, user_age, user_disease, user_gender];
+  const query = 'INSERT INTO user (user_id, user_password, user_name, user_age, user_disease, user_gender) VALUES (?, ?, ?, ?, ?)';
+  const values = [user_id, user_password, user_name, user_age, user_disease, user_gender];
 
   db.query(query, values, (error, results) => {
       if (error) {
